cc6c840792931ed7914ca5173d749d7fd8d0614d
JaviyWang <jwang1899@bths.edu>
2017-02-03 13:39:13 -0500

    Did some stuff with the monster

---
+++ TapMonster/src/monster/monsterAbilities.java
@@ -0,0 +1,5 @@
+package monster;
+
+public interface monsterAbilities {
+
+}
---
+++ TapMonster/src/monster/monsterTarget.java
@@ -0,0 +1,9 @@
+package monster;
+
+public interface monsterTarget {
+	void decreaseHearts(int i);
+
+	void subtractExtraLife(int i);
+
+	void decreaseTimer(int i);
+}
