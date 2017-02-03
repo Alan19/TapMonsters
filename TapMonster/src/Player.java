8443c5a5341ddf628fbaa0c6ed824f9e1cc369a1
simonblast0 <swk1124@gmail.com>
2017-02-03 13:34:10 -0500

    Added stuff

--- TapMonster/src/Player.java
+++ TapMonster/src/Player.java
@@ -1,7 +1,7 @@
 import guiPractice.GUIApplication;
 
 public class Player extends GUIApplication{
-
+public static String userInput;
 	public Player(int x, int y, int w, int h) {
 		
 		// TODO Auto-generated constructor stub
