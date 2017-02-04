c4c47907158664c1915b3e961d935785c1d9e328
swk11 <swk11@Leviathan>
2017-02-04 13:41:31 -0500

    added more stuff

--- TapMonster/src/Player.java
+++ TapMonster/src/Player.java
@@ -1,11 +1,18 @@
 import java.awt.Graphics2D;
-
+import java.util.List;
 import guiPractice.GUIApplication;
 import interfaces.PlayerInterface;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 public class Player extends GUIApplication implements PlayerInterface{
 public static final String userInput = new String();
-	public Player(int x, int y, int w, int h, String userInput) {
+public static final int playerX = 50;
+public static final int playerY = 50;
+
+	public Player(int x, int y, int scale, String userInput) {
+		x = playerX;
+		y = playerY;
 		Graphics2D playerGraphic;
 		// TODO Auto-generated constructor stub
 	}
