8b7566b6f1741d020cb7d2c0fa60d7f8955d7b3b
sidings <jrna.fng@gmail.com>
2017-02-05 23:04:10 -0500

    just created InventoryScreen and StoreScreen

---
+++ TapMonster/src/main/InventoryScreen.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.ArrayList;
+
+import guiPractice.Screen;
+import guiPractice.components.Visible;
+
+public class InventoryScreen extends Screen {
+
+	public InventoryScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
