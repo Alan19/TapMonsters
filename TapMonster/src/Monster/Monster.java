5a7d4f1edbe2ccd86481057223daa104a3055852
acuzzi4520 <acuzzi4520@bths.edu>
2017-02-07 13:51:09 -0500

    Update Monster.java

--- TapMonster/src/Monster/Monster.java
+++ TapMonster/src/Monster/Monster.java
@@ -108,7 +108,7 @@ public class Monster extends Component implements MonsterName{
 	private static String[] abilities = {"Godly","Frost","Pyro","Poison","Normal"};
 	private static String[] name1 = {"Terrifying","Big","Tiny","Dangerous","Angry","Cruel","Awesome","Friendly","Deathly","Lonely"};
 	private static String[] name2 = {"Calm","Hasty","Lazy","Casual","Caring","Emotional","Brave","Honest","Fluffy","Safe"};
-	private static String[] name3 = {"Beast","Fiend","Brute","Demon","Colossus","Savage","Monster","Animal","Thingy"};
+	private static String[] name3 = {"Beast","Humanoid","Demon","Titan","Monster","Thingy"};
 	private static ArrayList<String> names = new ArrayList<String>();
 
 	private String name;
