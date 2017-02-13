3c412536a3fbc03d562c0dfd9b338133a22bfcc6
Ahmed <Ahmed@LAPTOP-F21F4UUT.fios-router.home>
2017-02-12 19:06:58 -0500

    Store stuff and new component

--- TapMonster/src/main/AhmedDemo.java
+++ TapMonster/src/main/AhmedDemo.java
@@ -4,17 +4,18 @@ import guiPractice.GUIApplication;
 
 public class AhmedDemo extends GUIApplication{
 
-	public AhmedDemo() {
+	public AhmedDemo(int width, int height) {
+		super(width, height);
 	}
 
 	@Override
 	protected void initScreen() {
-		StoreScreen store = new StoreScreen(width, height);
+		StoreScreen store = new StoreScreen(getWidth(), getHeight());
 		setScreen(store);
 	}
 
 	public static void main(String[] args){
-		AhmedDemo game = new AhmedDemo();
+		AhmedDemo game = new AhmedDemo(2000, 2000);
 		Thread app = new Thread(game);
 		app.start();
 	}
