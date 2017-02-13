e2056c7c20f0616c677f9213dd365dd3a3632f01
Mahin <mkhan6241@bths.edu>
2017-02-12 20:02:04 -0500

    changes

--- TapMonster/src/main/MahinDemo.java
+++ TapMonster/src/main/MahinDemo.java
@@ -3,15 +3,9 @@
  */
 package main;
 
-import java.awt.Color;
-import java.util.ArrayList;
 
 import guiPractice.GUIApplication;
-import guiPractice.components.Action;
-import guiPractice.components.Button;
-import guiPractice.components.ClickableScreen;
-import guiPractice.components.TextLabel;
-import guiPractice.components.Visible;
+
 
 /**
  * @author Student 8
@@ -23,9 +17,8 @@ public class MahinDemo extends GUIApplication {
 	/**
 	 * 
 	 */
-	public MahinDemo() {
-		//construct new
-		//reward = new Reward;
+	public MahinDemo(int width, int height) {
+		super(width, height);
 	}
 
 	/* (non-Javadoc)
@@ -33,7 +26,10 @@ public class MahinDemo extends GUIApplication {
 	 */
 	@Override
 	protected void initScreen() {
-		DemoScreen demo = new DemoScreen(getWidth(),getHeight());
+		//DemoScreen demo = new DemoScreen(getWidth(),getHeight());
+		//setScreen(demo);
+		
+		InventoryScreen demo = new InventoryScreen(getWidth(), getHeight());
 		setScreen(demo);
 
 	}
@@ -42,42 +38,10 @@ public class MahinDemo extends GUIApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MahinDemo demo = new MahinDemo();
+		MahinDemo demo = new MahinDemo(300,300);
 		Thread app = new Thread(demo);
 		app.start();
 
-	}
-	
-	//nested inner class
-	private class DemoScreen extends ClickableScreen{
-
-		//private TextLabel rewardDisplay;
-		//private Button beatAMonster;
-		
-		public DemoScreen(int width, int height) {
-			super(width, height);
-			// TODO Auto-generated constructor stub
-		}
-
-		@Override
-		public void initAllObjects(ArrayList<Visible> view) {
-			/*rewardDisplay = new TextLabel(20,40,700,25,"");
-			beatAMonster = new Button(40,100,190,40,"Beat a monster", 
-					Color.blue, new Action() {
-						
-						@Override
-						public void act() {
-							MahinDemo.reward.getReward(10);
-							rewardDisplay.setText("You earned a reward!"
-									+ " Total points ="
-									+ "+MahinDemo.reward.getPoints());
-							
-						}
-					});
-			view.add(rewardDisplay);
-			view.add(beatAMonster);*/
-		}
-		
 	}
 
 }
