476ffc203e597a67366e49324a9e75e7d410a3e3
acuzzi4520 <acuzzi4520@bths.edu>
2017-02-16 14:02:33 -0500

    still need buttons to work

--- TapMonster/src/MonsterDex/MonsterDex.java
+++ TapMonster/src/MonsterDex/MonsterDex.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import guiPractice.ClickableScreen;
+import guiPractice.components.Action;
 import guiPractice.components.Button;
 import guiPractice.components.Graphic;
+import guiPractice.components.TextArea;
+import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import interfaces.MonDexInterface;
 
@@ -17,6 +20,8 @@ public class MonsterDex extends ClickableScreen implements Runnable{
 	private Button Back;
 	private Graphic Background;
 	private static Graphic MonsterImg;
+	private static TextArea Description;
+	private TextLabel Name;
 	
 	private static int PosX = 100;
 	private static int PosY = 100;
@@ -29,30 +34,29 @@ public class MonsterDex extends ClickableScreen implements Runnable{
 	}
 	
 	
-	public void initObjects(ArrayList<Visible> viewObjects){
+	public void initObjects(final ArrayList<Visible> viewObjects){
 		Monsters = new ArrayList<MonDexInterface>();
 		Next = new Button(getWidth() - 110, getHeight() - 70, 90, 50, "Next", Color.decode("#a52a2a"), null);
-		Back = new Button(20, getHeight() - 70, 90, 50, "Back", Color.decode("#a52a2a"), null);
+		Back = new Button(20, getHeight() - 70, 90, 50, "Back", Color.decode("#a52a2a"), new Action() {
+			
+			public void act() {
+				MonID++;
+			}
+		});
+		
 		Background = new Graphic(0,0,getWidth(),getHeight(),"src/resources/background.jpg");
 		MonsterImg = getMonster();
 		viewObjects.add(Background);
 		viewObjects.add(Next);
 		viewObjects.add(Back);	
 		viewObjects.add(MonsterImg);
-//		for(int i = MonID; i < MonID+1; i++){
-//			MonsterImg = new Graphic(PosX,PosY, 100,100,"src/MonsterSprites/"+MonID+".gif");
-//			PosX += 200;
-//			MonID++;
-//			viewObjects.add(MonsterImg);
-//		}
+
 	}
 
-
-	private MonsterGraphic getMonster() {
+	private Graphic getMonster() {
 		MonID++;
 		return new MonsterGraphic(PosX, PosY, 100, 100, MonID);
 	}
-
 
 	public void run() {
 		// TODO Auto-generated method stub
