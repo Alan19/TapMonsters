4d5efbb46ad1697c4e2e07687479d79da10d0deb
Alan19 <alanxiao44@gmail.com>
2017-02-27 13:52:00 -0500

    Redid MonsterDexInterfaces

---
+++ TapMonster/src/interfaces/MonDexScreenInterface.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package interfaces;
+
+import java.util.ArrayList;
+
+import MonsterDex.MonsterGraphic;
+
+/**
+ * @author Alan
+ *
+ */
+public interface MonDexScreenInterface {
+	ArrayList<MonsterGraphic> getMonsters();
+
+}
