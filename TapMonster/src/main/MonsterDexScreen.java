9b93e529f8a60cbedd3ee7f0ec1cb79cb7a9db12
acuzzi4520 <acuzzi4520@bths.edu>
2017-03-01 14:03:00 -0500

    Fixed Monster Dex

--- TapMonster/src/main/MonsterDexScreen.java
+++ TapMonster/src/main/MonsterDexScreen.java
@@ -12,17 +12,18 @@ import guiPractice.components.TextArea;
 import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import interfaces.MonDexInterface;
-import monsterDex.MonsterGraphic;
+import main.MonsterGraphic;
 
 public class MonsterDexScreen extends ClickableScreen implements Runnable,MonDexInterface{
 	
-	private static ArrayList<MonsterGraphic> Monsters;
+	private static ArrayList<Graphic> Monsters;
 
 	private Button next;
 	private Button back;
 	private Button home;
 	private Graphic background;
-	private static Graphic monsterImg;
+	private static Graphic monsterImg1;
+	private static Graphic monsterImg2;
 	private static TextArea Description;
 	private TextLabel Name;
 	
@@ -52,6 +53,14 @@ public class MonsterDexScreen extends ClickableScreen implements Runnable,MonDex
 			"They exist mainly in the other world, but can sometimes enter our world. When they do they can stir up any amount of trouble",
 			"The things are despicable creatures, whose appetites consist mainly of anything it can get its hands/tentacles/etc. on",
 			"It can hardly be called a creature, much less a 'thing'. Due to their rarity, they are difficult to fight, since few have seen them and even fewer kill them."};
+
+	private Graphic monsterImg3;
+
+	private Graphic monsterImg4;
+
+	private Graphic monsterImg5;
+
+	private Graphic monsterImg6;
 		
 	public MonsterDexScreen(int width,int height) {
 		super(1000, 800);
@@ -60,28 +69,10 @@ public class MonsterDexScreen extends ClickableScreen implements Runnable,MonDex
 	}
 	
 	
-	public void initObjects(final ArrayList<Visible> viewObjects){
-		next = new Button(getWidth() - 110, getHeight() - 70, 90, 50, "Next", Color.decode("#a52a2a"), null);
-		back = new Button(20, getHeight() - 70, 90, 50, "Back", Color.decode("#a52a2a"), new Action() {
-			public void act() {
-				//monsterImg.loadImages( "src/MonsterSprites/"++".gif", monsterImg.getWidth(), monsterImg.getHeight());
-			}
-		});
-		home = new Button(10,10,100,50, "Home", Color.red, new Action(){
-			public void act(){
-				TapMonsterGame.game.setScreen(TapMonsterGame.main);
-			}
-		});
-		background = new Graphic(0,0,getWidth(),getHeight(),"src/resources/background.jpg");
-		monsterImg = new Graphic(100,100,100,100,monsterDex.MonsterGraphic.getMonster("Beast")); 
-		viewObjects.add(background);
-		viewObjects.add(next);
-		viewObjects.add(back);	
-		viewObjects.add(home);	
-		viewObjects.add(monsterImg);
-
-	}
-
+//	public void initObjects(ArrayList<Visible> viewObjects){
+//		
+//	}
+//
 
 
 	public void run() {
@@ -112,19 +103,48 @@ public class MonsterDexScreen extends ClickableScreen implements Runnable,MonDex
 		return null;
 	}
 	
-	public static void setMonsters(ArrayList<MonsterGraphic> monsters) {
+	public static void setMonsters(ArrayList<Graphic> monsters) {
 		Monsters = monsters;
 	}
 
-	public ArrayList<MonsterGraphic> getMonsters() {
+	public ArrayList<Graphic> getMonsters() {
 		return Monsters;
 	}
 
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		next = new Button(getWidth() - 110, getHeight() - 70, 90, 50, "Next", Color.decode("#a52a2a"), null);
+		back = new Button(20, getHeight() - 70, 90, 50, "Back", Color.decode("#a52a2a"), new Action() {
+			public void act() {
+				monsterImg1.loadImages(main.MonsterGraphic.getMonster("Beast"), 100, 100);
+				monsterImg2.loadImages(main.MonsterGraphic.getMonster("Beast"), 100, 100);
+			}
+		});
+		home = new Button(10,10,100,50, "Home", Color.red, new Action(){
+			public void act(){
+				TapMonsterGame.game.setScreen(TapMonsterGame.main);
+			}
+		});
+		background = new Graphic(0,0,getWidth(),getHeight(),"src/resources/background.jpg");
+		monsterImg1 = new Graphic(100,100,100,100,main.MonsterGraphic.getMonster("Titan")); 
+		monsterImg2 = new Graphic(200,200,100,100,main.MonsterGraphic.getMonster("Beast")); 
+		monsterImg3 = new Graphic(300,300,100,100,main.MonsterGraphic.getMonster("Titan")); 
+		monsterImg4 = new Graphic(400,400,100,100,main.MonsterGraphic.getMonster("Beast")); 
+		monsterImg5 = new Graphic(500,500,100,100,main.MonsterGraphic.getMonster("Beast")); 
+		monsterImg6 = new Graphic(600,600,100,100,main.MonsterGraphic.getMonster("Titan")); 
 		
+		viewObjects.add(background);
+		viewObjects.add(next);
+		viewObjects.add(back);	
+		viewObjects.add(home);	
+		viewObjects.add(monsterImg1);
+		viewObjects.add(monsterImg2);
+		viewObjects.add(monsterImg3);
+		viewObjects.add(monsterImg4);
+		viewObjects.add(monsterImg5);
+		viewObjects.add(monsterImg6);
+
 	}
 
 }
---
+++ TapMonster/src/main/MonsterGraphic.java
@@ -0,0 +1,125 @@
+package main;
+
+import guiPractice.components.Graphic;
+
+public class MonsterGraphic extends Graphic{
+	
+	public static int MonX = 100;
+	public static int MonY = 100;
+	private static int randInt;
+	public static String tempname;
+	
+	public static String[] beast = {"Anguiform","Bismarck","Fidor","Harpiai","Nastidon","Palidor","Prussian","Red_Wolf","Sand_Horse","Toe_Cutter","Vulture","Were-Rat"};
+	public static String[] demon = {"Daredevil","Chupon","Crucifix",
+	"Doom Gaze","Harvester","Hidon","Shaman","Slam_Dancer","Steroidite","Tritoch","Trixter"};
+	public static String[] fiend = 	{"BioSoldier","Brawler","Guard","Hemophyte","Ninja","Parasoul","Pipsqueak","Samurai","ShadowDancer","Test Rider","Yojimbo"};
+	public static String[] monster = {"BoneDragon","Cactuar","Esper","Evil_Oscar","MagicPot","Sandcrawler","Uroburos","WhiteFlame",};
+	public static String[] thingy = {"Adamanchyt","Deep Eye","Flan","Mag Roadie","Poplium","Pug","Veteren","Wart Puck"};
+	public static String[] titan = {"#128","Atma Weapon","Behemoth","Borras","Chimera","Czar Dragon","Didalos","Kefka","Ultros","Zombone","Zone Eater"};
+	
+	
+	public MonsterGraphic(int x, int y, int w, int h, String species) {
+		super(x, y, getMonster(species));
+	}
+	
+	public static void SetX(int x){
+		MonX = x;
+	}
+	
+	public static void SetY(int y){
+		MonY = y;
+	}
+	
+	public int getX(){
+		return MonX;
+	}
+	public int getY(){
+		return MonY;
+	}
+	
+	public static String getMonster(String type){
+		System.out.println("Type:" + type);
+		if(type.equals("Beast")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Beast/"+beast[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;
+		}
+		
+		else if(type.equals("Demon")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Demon/"+demon[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;
+		}
+
+		else if(type.equals("Fiend")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Fiend/"+fiend[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;
+		}
+		
+		else if(type.equals("Monster")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Monster/"+monster[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;	
+			}
+
+		else if(type.equals("Thingy")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Thingy/"+thingy[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;
+			}
+		
+		else if(type.equals("Titan")){
+			getRandInt(type);
+			tempname = "MonsterSprites/Titan/"+titan[randInt]+".gif";
+			System.out.println(tempname);
+			return tempname;
+			}
+		
+		getRandInt(type);
+		
+		return "MonsterSprites/Beast/"+thingy[randInt]+".gif"; 
+	}
+
+	private static void getRandInt(String type) {
+		if(type == "Beast"){
+			randInt = (int) (Math.random()*beast.length);
+		}
+		
+		else if(type == "Demon"){
+			randInt = (int) (Math.random()*demon.length);
+		}
+
+		else if(type == "Fiend"){
+			randInt = (int) (Math.random()*fiend.length);
+		}
+		
+		else if(type == "Monster"){
+			randInt = (int) (Math.random()*monster.length);
+		}
+
+		else if(type == "Thingy"){
+			randInt = (int) (Math.random()*thingy.length);
+		}
+		
+		else if(type == "Titan"){
+			randInt = (int) (Math.random()*titan.length);
+		}
+//		randInt = (int) (Math.random()*12);
+	}
+	
+	public String getImagePath() {
+		return tempname;
+		
+	}
+
+	
+	
+	
+
+}
