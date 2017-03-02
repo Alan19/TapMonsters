d668a6460b786d39b00c99a6175c37fc12d419c5
Alan Xiao <alanxiao44@gmail.com>
2017-03-01 22:55:49 -0500

    Updated Interface

--- TapMonster/src/interfaces/MonDexInterface.java
+++ TapMonster/src/interfaces/MonDexInterface.java
@@ -1,18 +1,23 @@
 package interfaces;
 
+import java.util.ArrayList;
+
 import guiPractice.components.Clickable;
+import main.Monster;
 
 public interface MonDexInterface{
 		
+	
 	void SetDescription(String des);
 	
 	int setNumEncountered(int encounters);
 
-	
 	/**
 	 *@Alan
 	 */
 	String getImagePath();
 
 	String getName();
+	
+	ArrayList<Monster> getMonsters();
 }
