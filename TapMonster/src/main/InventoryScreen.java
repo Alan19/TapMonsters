a2d7c895614f6f836cdcea35a590896d2dc0cc5f
Mahin <mkhan6241@bths.edu>
2017-03-02 22:42:17 -0500

    back button should work

--- TapMonster/src/main/InventoryScreen.java
+++ TapMonster/src/main/InventoryScreen.java
@@ -70,7 +70,7 @@ public class InventoryScreen extends ClickableScreen implements Runnable, Invent
 		bkgd = new Graphic(0,0,getWidth(),getHeight(),"src/resources/background.jpg");
 		back = new Button(getWidth()/120,getWidth()/120,100,50, "Back", Color.red, new Action(){
 			public void act(){
-				//back to main screen
+				TapMonsterGame.game.setScreen(TapMonsterGame.main);
 			}
 		});
 		
