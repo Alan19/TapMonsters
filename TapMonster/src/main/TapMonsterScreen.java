88fa716c06c580301353042846e9550abf16ca11
Alan Xiao <alanxiao44@gmail.com>
2017-03-21 20:05:27 -0400

    Revert "Beta test"
    This reverts commit fd94fb05dfde1bd819bb907f13a5b7201962b0d6.

--- TapMonster/src/main/TapMonsterScreen.java
+++ TapMonster/src/main/TapMonsterScreen.java
@@ -48,7 +48,6 @@ public class TapMonsterScreen extends ClickableScreen implements Runnable,Game,R
 	private static Reward rewardObject;
 	private static Reward[] rewardList;
 	public static boolean setWasSequenceCompleted;
-	public static SequenceScreen2 newScreen;
 	
 	private boolean wasSequenceCompleted;
 	private boolean sequenceNotCompleted;
@@ -224,7 +223,7 @@ public class TapMonsterScreen extends ClickableScreen implements Runnable,Game,R
 
 				resetTimer();
 				TapMonsterGame.game.setScreen(TapMonsterGame.fightScreen);
-				newScreen = new SequenceScreen2(getWidth(), getHeight());
+				
 			}
 		});
 
